b0bcd9221799dec8fe9e1475649b3db9eb62ee02
srikanthg199 <srikanthdevarakonds@gmail.com>
2025-01-06 19:08:05 +0530

    Updated permissions in task module

--- get-started/src/task/task.controller.ts
+++ get-started/src/task/task.controller.ts
@@ -14,23 +14,31 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 import { GetUser } from 'src/decorators/get-user.decorator';
 import { User } from 'src/user/user.entity';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { PermissionsGuard } from 'src/auth/guards/permissions.guard';
+import { RequiredPermission } from 'src/decorators/required-permission.decorator';
 
 @Controller('task')
+@UseGuards(JwtAuthGuard)
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(PermissionsGuard)
+  @RequiredPermission('CREATE_TASK')
   create(@Body() createTaskDto: CreateTaskDto, @GetUser() user: User) {
     return this.taskService.create(createTaskDto, user);
   }
 
   @Get()
+  // @UseGuards(PermissionsGuard)
+  @RequiredPermission('FETCH_TASKS')
   findAll() {
     return this.taskService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(PermissionsGuard)
+  @RequiredPermission('VIEW_TASK')
   async findOne(@Param('id') id: string) {
     const data = await this.taskService.findOne(id);
     console.log(/dd/, data);
@@ -38,11 +46,15 @@ export class TaskController {
   }
 
   @Patch(':id')
+  @UseGuards(PermissionsGuard)
+  @RequiredPermission('UPDATE_TASK')
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.taskService.update(+id, updateTaskDto);
   }
 
   @Delete(':id')
+  @UseGuards(PermissionsGuard)
+  @RequiredPermission('DELETE_TASK')
   remove(@Param('id') id: string) {
     return this.taskService.remove(+id);
   }
